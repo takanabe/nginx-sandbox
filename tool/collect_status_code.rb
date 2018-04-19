ee4ce5cf2bbf18a2f571b0f3be5fc5acc3e74c49
Takayuki WATANABE <takayuki-watanabe@cookpad.com>
2018-04-20 05:11:54 +0900

    Switch to green

--- tool/collect_status_code.rb
+++ tool/collect_status_code.rb
@@ -4,12 +4,15 @@ status_count = {}
 
 Signal.trap(:INT){
   puts "SIGINT"
+  status_count.each do |k,v|
+    puts "status: #{k}, count: #{v}"
+  end
   exit(0)
 }
 
 loop do
   uri = URI("http://localhost:10080")
   res = Net::HTTP.get_response(uri)
-  puts "Status: #{res.code}"
+  puts "#{res['X-SANDBOX-ENV']} app,  Status: #{res.code}"
   status_count[res.code] = status_count.key?(res.code) ?  status_count[res.code] + 1 : 1
 end

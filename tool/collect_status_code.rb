3b6f9e1a29989f5ece839741c7ba866bc7fc5a01
Takayuki WATANABE <takayuki-watanabe@cookpad.com>
2018-04-24 23:52:43 +0100

    Switch to Blue

--- tool/collect_status_code.rb
+++ tool/collect_status_code.rb
@@ -3,14 +3,6 @@ require 'net/http'
 status_count = {}
 port = ENV.fetch("NGINX_PORT", "10080")
 
-Signal.trap(:INT){
-  puts "SIGINT"
-  status_count.each do |k,v|
-    puts "Total: status: #{k}, count: #{v}"
-  end
-  exit(0)
-}
-
 loop do
   begin
     uri = URI("http://localhost:#{port}")
@@ -18,6 +10,13 @@ loop do
 
     puts "#{Time.now } #{res['X-SANDBOX-ENV']} app,  Status: #{res.code}"
     status_count[res.code] = status_count.key?(res.code) ?  status_count[res.code] + 1 : 1
+    sleep 3
+  rescue SignalException
+    puts "SIGINT"
+    status_count.each do |k,v|
+      puts "Total: status: #{k}, count: #{v}"
+    end
+    exit(0)
   rescue Exception => msg
     #puts "error msg: #{msg}"
     puts "Switch Blue/Green app"

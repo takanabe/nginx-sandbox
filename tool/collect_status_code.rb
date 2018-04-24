2f571f5a8042a326a115f5ee72d5794796a06132
Takayuki WATANABE <takayuki-watanabe@cookpad.com>
2018-04-24 23:37:05 +0100

    Switch to Green

--- tool/collect_status_code.rb
+++ tool/collect_status_code.rb
@@ -12,8 +12,13 @@ Signal.trap(:INT){
 }
 
 loop do
-  uri = URI("http://localhost:#{port}")
-  res = Net::HTTP.get_response(uri)
-  puts "#{Time.now } #{res['X-SANDBOX-ENV']} app,  Status: #{res.code}"
-  status_count[res.code] = status_count.key?(res.code) ?  status_count[res.code] + 1 : 1
+  begin
+    uri = URI("http://localhost:#{port}")
+    res = Net::HTTP.get_response(uri)
+
+    puts "#{Time.now } #{res['X-SANDBOX-ENV']} app,  Status: #{res.code}"
+    status_count[res.code] = status_count.key?(res.code) ?  status_count[res.code] + 1 : 1
+  rescue Exception => msg
+    puts "error msg: #{msg}"
+  end
 end
